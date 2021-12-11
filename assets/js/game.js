b3e88dbe201d36cc61fdd3da317a34057d3897c1
ganbari <steinway.nathan@gmail.com>
2021-12-10 20:40:18 -0600

    added highscore function

--- assets/js/game.js
+++ assets/js/game.js
@@ -163,6 +163,26 @@ var endGame = function() {
       else {
         window.alert("You've lost your robot in battle.");
       }
+      
+    var highScore = localStorage.getItem("highscore");
+        if (highScore === null) {
+            highScore = 0;
+        }
+        
+        if (playerInfo.money > highScore) {
+
+        localStorage.setItem("highscore", playerInfo.money);
+
+        localStorage.setItem("name", playerInfo.name);
+
+        alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+
+        } 
+        else {
+
+        alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
+        
+        }
 
     var playAgainConfirm = window.confirm("Would you like to play again?");
 
